581c740f33a1e35ca1f5d491dff5eb553f50ddeb
Zulkar Nine <gmzulkar@gmail.com>
2021-11-17 16:25:48 +0600

    requirements updated

--- TFHRM/settings.py
+++ TFHRM/settings.py
@@ -182,10 +182,14 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'User_App.User'
 
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
