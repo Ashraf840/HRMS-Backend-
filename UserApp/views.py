b7c75edeec914502060104a33ec56dd7dad901af
Zulkar Nine <gmzulkar@gmail.com>
2021-11-21 16:05:08 +0600

    user info bug

--- UserApp/views.py
+++ UserApp/views.py
@@ -16,19 +16,6 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 
 
 
-# # JWT session destroy and blacklist tokens
-# class LogoutView(views.APIView):
-#     permission_classes = (permissions.IsAuthenticated,)
-#
-#     def post(self, request):
-#         try:
-#             refresh_token = request.data["refresh_token"]
-#             token = RefreshToken(refresh_token)
-#             token.blacklist()
-#
-#             return response.Response(status=status.HTTP_205_RESET_CONTENT)
-#         except Exception as e:
-#             return response.Response(status=status.HTTP_400_BAD_REQUEST)
 
 # User Registration view inheriting APIView class
 class RegisterView(generics.CreateAPIView):
