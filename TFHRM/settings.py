43e129608413188d20b74fa52a43e1b7f81d4891
Zulkar Nine <gmzulkar@gmail.com>
2021-12-26 12:34:31 +0600

    document submission model change

--- TFHRM/settings.py
+++ TFHRM/settings.py
@@ -132,12 +132,12 @@ ROOT_URLCONF = 'TFHRM.urls'
 # ============== sqlite3 Db ==============
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # ============== postgresql Db ==============
@@ -157,21 +157,21 @@ Dev server DB
 """
 
 # ============== mysql Db ==============
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'hrm_techforing',
-         'USER': 'root',
-         'PASSWORD': 'hOLL4m&*%$',
-         'OPTIONS': {
-             'sql_mode': 'traditional',
-         }
-     }
- }
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.mysql',
+#          'NAME': 'hrm_techforing',
+#          'USER': 'root',
+#          'PASSWORD': 'hOLL4m&*%$',
+#          'OPTIONS': {
+#              'sql_mode': 'traditional',
+#          }
+#      }
+#  }
 
 # ============== securing proxy for heroku hosting ==============
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
