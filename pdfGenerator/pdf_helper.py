5f7b5e046c505e1986a4da4de93858bb79e1cd4c
unknown <gmzulkar@gmail.com>
2022-02-01 18:42:34 +0600

    removed all file

--- pdfGenerator/pdf_helper.py
+++ pdfGenerator/pdf_helper.py
@@ -13,7 +13,7 @@ def save_pdf(params: dict):
     pdf = pisa.pisaDocument(BytesIO(html.encode('UTF-8')), response)
     file_name = uuid.uuid4()
     try:
-        with open(f'{settings.BASE_DIR}media/OfficialDocuments/{file_name}.pdf', 'wb+') as output:
+        with open(str(settings.BASE_DIR)+f'media/OfficialDocuments/{file_name}.pdf', 'wb+') as output:
             pdf = pisa.pisaDocument(BytesIO(html.encode('UTF-8')), output)
 
     except Exception as e:
