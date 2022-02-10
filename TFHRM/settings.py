1e5ec3a6d919852b2a31cc58b404cd7a863a699f
unknown <gmzulkar@gmail.com>
2022-02-10 12:13:50 +0600

    check

--- TFHRM/settings.py
+++ TFHRM/settings.py
@@ -39,13 +39,13 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '0.0.0.0',
     '44.242.38.198',
-    'careeradmin.techforing.com',
-    'career.techforing.com',
-    'hrms.techforing.com',
-    '127.0.0.1',
-    '127.0.0.1:8000',
-    'localhost:3001',
-    'localhost:3000',
+    'https://careeradmin.techforing.com',
+    'https://career.techforing.com',
+    'https://hrms.techforing.com',
+    'http://127.0.0.1',
+    'http://127.0.0.1:8000',
+    'http://localhost:3001',
+    'http://localhost:3000',
 ]
 
 # Initialise environment variables
@@ -265,8 +265,8 @@ MEDIA_ROOT = BASE_DIR.joinpath('media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# CORS_ORIGIN_ALLOW_ALL = False
-CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = False
+# CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     '0.0.0.0',
     '44.242.38.198',
@@ -277,7 +277,6 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3001',
     'http://localhost:3000',
-
 ]
 
 import os
