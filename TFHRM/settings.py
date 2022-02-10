27233e3577a857e1f116688b8dcf6deaf88dc00a
unknown <gmzulkar@gmail.com>
2022-02-10 13:14:12 +0600

    cors changes

--- TFHRM/settings.py
+++ TFHRM/settings.py
@@ -265,7 +265,7 @@ MEDIA_ROOT = BASE_DIR.joinpath('media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ORIGIN_ALLOW_ALL = False
+# CORS_ORIGIN_ALLOW_ALL = False
 # CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://0.0.0.0',
@@ -277,6 +277,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3001',
     'http://localhost:3000',
+    'http://localhost:8000',
 ]
 
 import os
