200ad693a3edb578ad01beecbf371b30a9d22b7a
unknown <gmzulkar@gmail.com>
2022-03-01 13:12:56 +0600

    Filter question field type custome option functionality added

--- RecruitmentManagementApp/admin.py
+++ RecruitmentManagementApp/admin.py
@@ -13,18 +13,21 @@ class StatusModel(admin.ModelAdmin):
         order = ['status']
 
 
-admin.site.register(models.JobPostModel)
-admin.site.register(models.UserJobAppliedModel)
-admin.site.register(models.FilterQuestionsResponseModelHR)
+admin.site.register(models.JobPostModel)  # job post model
+admin.site.register(models.UserJobAppliedModel)  # job application model
+# online, practical test section
 admin.site.register(models.OnlineTestModel)
 admin.site.register(models.PracticalTestModel)
 admin.site.register(models.PracticalTestResponseModel)
 admin.site.register(models.OnlineTestResponseModel)
-admin.site.register(models.JobStatusModel, StatusModel)
+admin.site.register(models.JobStatusModel, StatusModel)  # job status model
+# Official document, references submission section
 admin.site.register(models.DocumentSubmissionModel)
 admin.site.register(models.ReferenceInformationModel)
 admin.site.register(models.OfficialDocumentsModel)
+# filter questions sections
 admin.site.register(models.FieldTypeModels)
 admin.site.register(models.JobApplyFilterQuestionModel)
 admin.site.register(models.FilterQuestionAnswerModel)
-
+admin.site.register(models.FilterQuestionsResponseModelHR)
+admin.site.register(models.JobFilterQuestionRadioButtonOptionModel)
