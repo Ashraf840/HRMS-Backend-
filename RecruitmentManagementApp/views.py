0e293709dc92c324f0054f8bf3cb8dd816535fd8
unknown <gmzulkar@gmail.com>
2022-03-02 18:25:25 +0600

    withdraw application

--- RecruitmentManagementApp/views.py
+++ RecruitmentManagementApp/views.py
@@ -869,7 +869,7 @@ class WithdrawApplicationView(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.UserJobAppliedModel.objects.all()
     lookup_field = 'id'
 
-    def update(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         applicationData = models.UserJobAppliedModel.objects.get(id=self.kwargs['id'], userId=self.request.user)
         status = models.JobStatusModel.objects.all()
         applicationData.jobProgressStatus = status.get(status='Withdrawn')
