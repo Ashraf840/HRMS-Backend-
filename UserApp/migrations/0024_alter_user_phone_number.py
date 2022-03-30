7d24b347292028f5b6f52e7d6ff725d7c9321984
unknown <gmzulkar@gmail.com>
2022-03-30 18:48:04 +0600

    add production

--- UserApp/migrations/0024_alter_user_phone_number.py
+++ UserApp/migrations/0024_alter_user_phone_number.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='user',
             name='phone_number',
-            field=models.CharField(max_length=15),
+            field=models.CharField(max_length=255),
         ),
     ]
