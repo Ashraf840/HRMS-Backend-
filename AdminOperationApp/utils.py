abcbf408b8f4cde5a41e39ecb76441257181f29e
faruk <faruk.techforing@gmail.com>
2022-04-26 11:56:24 +0600

    Added validation in policy sent and sending hr_policy pdf as well as solved the doc permission in  adminoperation view

--- AdminOperationApp/utils.py
+++ AdminOperationApp/utils.py
@@ -8,7 +8,11 @@ class Util:
         email = EmailMessage(
             subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
         email.send()
-
+    def send_email_attach(data):
+        email = EmailMessage(
+            subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
+        email.attach_file(data['file_path'])
+        email.send()
 
 async def send_mail2(data):
     email = EmailMessage(
